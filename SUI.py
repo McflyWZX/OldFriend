276360688ecae19267c02b1ea841023dd1d8f24b
Mcfly <coolmcfly@qq.com>
2025-03-03 20:20:01 +0800

    SUI代码更新，新增ItemList和注释

--- SUI.py
+++ SUI.py
@@ -12,11 +12,21 @@ Description: SUI(Sound user interface)，是纯声音用户交互的实现。
 from SoundManager import SoundManager
 from TTS_manager import TTS_manager
 
+'''
+description: 提供SUI的创建和管理功能，负责SUI内部控件数据流传递
+'''
 class SUI:
     def __init__(self, soundMgr:SoundManager, TTS_mgr:TTS_manager):
         self.soundMgr = soundMgr
         self.TTS_mgr = TTS_mgr
+        self.__visitList = None
 
+    def enterVisitList(self, itemList:"ItemList"):
+        self.__visitList = itemList
+
+'''
+description: 交互式控件的基类
+'''
 class Control:
     def __init__(self, UI_mgr:SUI):
         self.UI_mgr = UI_mgr;
@@ -27,6 +37,9 @@ class Control:
     def onEnter(self):
         pass
 
+'''
+description: 表项控件，当被选中时，默认播报标项名字
+'''
 class Item(Control):
     def __init__(self, UI_mgr:SUI, title='未知栏目'):
         super().__init__(UI_mgr)
@@ -38,3 +51,23 @@ class Item(Control):
 
     def onEnter(self):
         pass
+
+'''
+description: 表项列表控件，当被选中时，默认播报列表的名字，拥有一个管理标项的list
+'''
+class ItemList(Control):
+    def __init__(self, UI_mgr:SUI, title='未知栏目'):
+        super().__init__(UI_mgr)
+        self.title = title
+        self.items:list[Item] = []
+
+    def onSelect(self):
+        super().onSelect()
+
+    def onEnter(self):
+        if len(self.items) == 0:
+            # TODO: 播放“当前分类无内容”
+            return
+        # 将SUI当前正在浏览的列表替换  
+        self.UI_mgr.enterVisitList(self)
+        self.items[0].onSelect()

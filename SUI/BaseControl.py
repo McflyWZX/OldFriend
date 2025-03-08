d58bf9df341f0ce4e4b3e8b0265fe1063676c8da
Mcfly <coolmcfly@qq.com>
2025-03-08 16:49:03 +0800

    模块路径调整

---
+++ SUI/BaseControl.py
@@ -0,0 +1,76 @@
+'''
+Author: Mcfly coolmcfly@qq.com
+Date: 2025-03-08 16:32:06
+LastEditors: Mcfly coolmcfly@qq.com
+LastEditTime: 2025-03-08 16:43:44
+FilePath: \OldFriend\SUI\BaseControl.py
+Description: SUI模块内的控件子模块，定义了
+             列表、选项、快捷按钮三种交互控件及基础控件
+'''
+from ..SoundManager import SoundManager
+from ..TTS_manager import TTS_manager
+from typing import Callable
+from pynput.keyboard import Key
+import Manager
+
+'''
+description: 交互式控件的基类
+'''
+class Control:
+    def __init__(self, UI_mgr: Manager.SUI):
+        self.UI_mgr = UI_mgr;
+        self.keyMap: dict[Key, Callable[[], None]] = None
+
+    def onSelect(self):
+        pass
+
+    def onEnter(self):
+        pass
+
+    def getNewKeyMap(self):
+        return self.keyMap
+
+'''
+description: 表项控件，当被选中时，默认播报标项名字
+'''
+class Item(Control):
+    def __init__(self, UI_mgr: Manager.SUI, title='未知栏目'):
+        super().__init__(UI_mgr)
+        self.title = title
+
+    def onSelect(self):
+        audio = self.UI_mgr.TTS_mgr.tts(self.title)
+        self.UI_mgr.soundMgr.insVoiceAnnc(audio)
+
+    def onEnter(self):
+        pass
+
+'''
+description: 表项列表控件，当被选中时，默认播报列表的名字，拥有一个管理标项的list
+'''
+class ItemList(Control):
+    def __init__(self, UI_mgr: Manager.SUI, title='未知栏目'):
+        super().__init__(UI_mgr)
+        self.title = title
+        self.items:list[Item] = []
+
+    def onSelect(self):
+        super().onSelect()
+
+    def onEnter(self):
+        if len(self.items) == 0:
+            # TODO: 播放“当前分类无内容”
+            return
+        # 将SUI当前正在浏览的列表替换  
+        self.UI_mgr.enterVisitList(self)
+        self.items[0].onSelect()
+
+
+class QuickButton(Control):
+    def __init__(self, UI_mgr: Manager.SUI, title='未知栏目'):
+        super().__init__(UI_mgr)
+        self.title = title
+
+    def onEnter(self):
+        audio = self.UI_mgr.TTS_mgr.tts(self.title)
+        self.UI_mgr.soundMgr.insVoiceAnnc(audio)

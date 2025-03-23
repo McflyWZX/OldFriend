2ba2804b70d035aa06c10806140a07b91d30f6d6
Mcfly <coolmcfly@qq.com>
2025-03-23 18:00:21 +0800

    SoundManager实现及TTS_Manager实现上库（TTS_Manager还为完成，只完成text到hash值对应及持久化部分）

--- SoundManager.py
+++ SoundManager.py
@@ -2,17 +2,59 @@
 Author: Mcfly coolmcfly@qq.com
 Date: 2025-02-26 21:09:02
 LastEditors: Mcfly coolmcfly@qq.com
-LastEditTime: 2025-02-26 21:23:03
+LastEditTime: 2025-03-23 16:34:58
 FilePath: \GitClone\OldFriend\SoundManager.py
 Description: 声音播放管理系统，实现了主声音的播放暂停和连续播功能及操作声插播功能
 '''
+import pygame
+import time
+
 class SoundManager:
     def __init__(self):
-        pass
+        self.mainMusicPath = None
+        self.pause = True
+        self.playPos = 0
+        pygame.mixer.init()
+        pygame.init()
+
+    def playMainMusic(self, musicPath: str = None):
+        self.mainMusicPath = musicPath if musicPath is not None else self.mainMusicPath
+        if self.mainMusicPath is None:
+            print('Info: 尝试在没指定路径的情况下播放音乐')
+            return
+        pygame.mixer.music.load(self.mainMusicPath)
+        pygame.mixer.music.play(-1)  # -1 表示无限循环
+
+    def pause(self):
+        if self.mainMusicPath is None or self.pause is True:
+            return
+        # 手动控制暂停位置的方法，这里我们直接调用库函数
+        # mian_music_pos_g += (pygame.mixer.music.get_pos() + FADE_DURATION / 5) / 1000.0
+        # pygame.mixer.music.fadeout(FADE_DURATION)
+        self.playPos = pygame.mixer.music.get_pos() / 1000.0
+        pygame.mixer.music.pause()
+        self.pause = True
+
+    def resume(self):
+        if self.mainMusicPath is None or self.pause is False:
+            return
+        # pygame.mixer.music.load(MAIN_MUSIC)
+        # pygame.mixer.music.play(start=mian_music_pos_g, fade_ms=FADE_DURATION)
+        pygame.mixer.music.unpause()
+        self.pause = False
+
     '''
     description: 插入一条语音播报，立即播放
     param {*} self、annc：语音播报音频文件
     return {*}
     '''    
-    def insVoiceAnnc(self, annc):
-        print(annc)
+    def insVoiceAnnc(self, anncPath: str):
+        self.pause()
+        # 加载并播放音效
+        annc = pygame.mixer.Sound(anncPath)
+        annc.play()
+        
+        # 等待音效播放完成
+        while annc.get_num_channels() > 0:
+            time.sleep(0.01)
+        self.resume
--- TTS_manager.py
+++ TTS_manager.py
@@ -2,13 +2,60 @@
 Author: Mcfly coolmcfly@qq.com
 Date: 2025-02-26 21:47:14
 LastEditors: Mcfly coolmcfly@qq.com
-LastEditTime: 2025-02-26 21:53:36
+LastEditTime: 2025-03-23 17:45:20
 FilePath: \GitClone\OldFriend\TTS_manager.py
 Description: TTS管理模块，黑盒化文字键值转语音，内部基于记忆化搜索和TTS服务递归，仅在有需要时请求TTS服务，减小开销
 '''
+import sqlite3
+import hashlib
+import os
+
+TTS_PATH = 'tts/'
 
 class TTS_manager:
     def __init__(self):
         pass
+
     def tts(self, txt):
-        return txt
+        return '播报：' + txt
+    
+    def __initTTSdb(self, ttsDbPath):
+        # 数据库用于储存hash值与原文的对应关系
+        self.ttsDb = sqlite3.connect(ttsDbPath)
+        self.ttsDb.execute('''CREATE TABLE IF NOT EXISTS tts_files
+                        (hash_id TEXT PRIMARY KEY,
+                        original_text TEXT NOT NULL)''')
+        
+    def __getHash(self, text: str) -> str:
+        return hashlib.sha256(text.encode()).hexdigest()
+    
+    def saveTxtHash(self, text: str, txtHash: str):
+        cursor = self.ttsDb.cursor()
+        cursor.execute("SELECT original_text FROM tts_files WHERE hash_id=?", (txtHash,))
+        row = cursor.fetchone()
+        if row:
+            if row[0] != text:
+                raise Exception(f"哈希冲突！'{text}' 与 '{row[0]}'")
+            return  # 已存在
+        cursor.execute("INSERT INTO tts_files VALUES (?, ?)",
+                    (txtHash, text))
+        self.ttsDb.commit()
+
+    def isTxtHashExitst(self, text: str, txtHash: str) -> bool:
+        cursor = self.ttsDb.cursor()
+        cursor.execute("SELECT original_text FROM tts_files WHERE hash_id=?", (txtHash,))
+        row = cursor.fetchone()
+        if row:
+            if row[0] != text:
+                # 哈西冲突了，直接把源文件和db里的记录覆盖掉，这里返回不存在
+                print(f"哈希冲突！'{text}' 与 '{row[0]}'")
+                return False
+            return True
+    
+    def saveTxtHash(self, text: str):
+        txtHash = self.__getHash(text)
+        self.ttsDb.execute(
+            "INSERT OR REPLACE INTO tts_files VALUES (?, ?)",
+            (txtHash, text)
+        )
+        self.ttsDb.commit()
